572e09aa60ff705c4c78cb177f2a66079e09dd16
mrrsmburu <mrrsmburu@gmail.com>
2024-06-01 15:08:38 +0300

    completed assignment

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -1,56 +1,17 @@
-from datetime import datetime
-class Student:
-    def __init__(self, name):
-        self.name = name
-        self._enrollments = []
+def course_count(self):
+    return len(self._enrollments)
+@classmethod
+def aggregate_enrollments_per_day(cls):
+    enrollment_count = {}
+    for enrollment in cls.all:
+        date = enrollment.get_enrollment_date().date()
+        enrollment_count[date] = enrollment_count.get(date, 0) + 1
+    return enrollment_count
 
-    def enroll(self, course):
-        if isinstance(course, Course):
-            enrollment = Enrollment(self, course)
-            self._enrollments.append(enrollment)
-            course.add_enrollment(enrollment)
-        else:
-            raise TypeError("course must be an instance of Course")
+def aggregate_average_grade(self):
+      # lets assume the grades are stored in a protected attribute called _grades. 
+      total_grades = sum(self._grades.values())
+      num_courses = len(self._grades)
+      average_grade = total_grades / num_courses
 
-    def get_enrollments(self):
-        return self._enrollments.copy()
-
-class Course:
-    def __init__(self, title):
-
-        self.title = title
-        self._enrollments = []
-
-    def add_enrollment(self, enrollment):
-        if isinstance(enrollment, Enrollment):
-            self._enrollments.append(enrollment)
-        else:
-            raise TypeError("enrollment must be an instance of Enrollment")
-
-    def get_enrollments(self):
-        return self._enrollments.copy()
-
-
-class Enrollment:
-    all = []
-    
-    def __init__(self, student, course):
-        if isinstance(student, Student) and isinstance(course, Course):
-            self.student = student
-            self.course = course
-            self._enrollment_date = datetime.now()
-            type(self).all.append(self)
-        else:
-            raise TypeError("Invalid types for student and/or course")
-
-    def get_enrollment_date(self):
-        return self._enrollment_date
-    
-    def aggregate_average_grade(self):
-    total_grade = sum(self._grades.values())
-    num_courses = len(self._grades)
-    if num_courses == 0:
-    return 0  # Avoid division by zero error
-    average_grade = total_grades / num_courses
-    return average_grade
-
+      return average_grade
